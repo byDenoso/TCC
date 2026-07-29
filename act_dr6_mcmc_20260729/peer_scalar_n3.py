eae001516d33c4419affb446c0f5ad0b0dcf9c4c
byDenoso <denosooo2@gmail.com>
2026-07-29 08:18:20 -0300

    feat: add matched PEER CAMB theory wrapper

---
+++ act_dr6_mcmc_20260729/peer_scalar_n3.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from cobaya.theories.camb import camb as cobaya_camb
+from cobaya.theories.camb.camb import CambTransfers
+
+import camb
+from camb import dark_energy
+
+
+class PEERCambTransfers(CambTransfers):
+    def get_can_support_params(self):
+        return set(super().get_can_support_params()) | {"peer_fede", "peer_zc", "peer_thetai"}
+
+
+class PEERScalarN3(cobaya_camb.CAMB):
+    """CAMB EarlyQuintessence wrapper for the fixed-shape PEER1P model."""
+
+    params = {"peer_fede": None, "peer_zc": None, "peer_thetai": 2.89155}
+
+    def initialize(self):
+        super().initialize()
+        self._original_camb_set_params = self.camb.set_params
+
+    def get_can_support_params(self):
+        return super().get_can_support_params() + ["peer_fede", "peer_zc", "peer_thetai"]
+
+    def get_helper_theories(self):
+        self._camb_transfers = PEERCambTransfers(
+            self, "camb.transfers", {"stop_at_error": self.stop_at_error}, timing=self.timer
+        )
+        self._camb_transfers.requires = self._transfer_requires
+        return {"camb.transfers": self._camb_transfers}
+
+    def set(self, params_values_dict, state):
+        values = dict(params_values_dict)
+        fede = float(values.pop("peer_fede", 0.0))
+        logzc = float(values.pop("peer_zc", 3.81))
+        thetai = float(values.pop("peer_thetai", 2.89155))
+        original = self._original_camb_set_params
+
+        def patched_set_params(*args, **kwargs):
+            pars = original(*args, **kwargs)
+            if fede <= 1e-10:
+                return pars
+            ede = dark_energy.EarlyQuintessence()
+            ede.n = 3
+            ede.fde_zc = fede
+            ede.zc = 10.0 ** logzc
+            ede.theta_i = thetai
+            pars.DarkEnergy = ede
+            return pars
+
+        self.camb.set_params = patched_set_params
+        return super().set(values, state)
