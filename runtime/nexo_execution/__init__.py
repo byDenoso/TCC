2c3477ace8e8468f01e616a64f1e091340d1866a
byDenoso <denosooo2@gmail.com>
2026-09-11 17:52:05 -0300

    feat(nexo): add execution runtime package to dispatch branch

---
+++ runtime/nexo_execution/__init__.py
@@ -0,0 +1,9 @@
+from .core import ExecutionContract, ExecutionResult, GitHubActionsProvider, LocalProvider, ResultVerifier
+
+__all__ = [
+    "ExecutionContract",
+    "ExecutionResult",
+    "GitHubActionsProvider",
+    "LocalProvider",
+    "ResultVerifier",
+]
