7168ee7061e82e0f578a92722bbe90b7a7eb1d49
byDenoso <denosooo2@gmail.com>
2026-09-13 01:22:30 +0000

    test: model actual PolyChord 1.20.1 release bootstrap

--- peer_decisive_followups/test_polychord_bootstrap_patch.py
+++ peer_decisive_followups/test_polychord_bootstrap_patch.py
@@ -23,17 +23,31 @@ def _fixture_source() -> str:
     subroutine GenerateLivePoints(loglikelihood,prior,settings,RTI,mpi_information)
         use settings_module,  only: program_settings
         use random_module,   only: random_reals
-        use run_time_module, only: run_time_info,initialise_run_time_info, find_min_loglikelihoods
-        use array_module, only: add_point
+        use utils_module,    only: write_phys_unit,DB_FMT,fmt_len,minpos,time
+        use calculate_module, only: calculate_point
+        use read_write_module, only: phys_live_file, prior_info_file
+        use feedback_module,  only: write_started_generating,write_finished_generating,write_generating_live_points
+        use run_time_module,   only: run_time_info,initialise_run_time_info, find_min_loglikelihoods
+        use array_module,     only: add_point
+        use abort_module
+#ifdef MPI
+        use mpi_module, only: mpi_bundle,is_root,linear_mode,throw_point,catch_point,more_points_needed,sum_integers,sum_doubles,request_point,no_more_points
+#else
+        use mpi_module, only: mpi_bundle,is_root,linear_mode
+#endif
         implicit none
-        integer :: nlike
+        integer :: nlike ! number of likelihood calls
         integer :: nprior, ndiscarded
-        integer :: ngenerated ! use to track order points are generated in
         real(dp) :: time0,time1,total_time
+        real(dp),dimension(size(settings%grade_dims)) :: speed
         nlike = 0
-        ngenerated = 1
         if(is_root(mpi_information)) then
             call initialise_run_time_info(settings,RTI)
+        end if
+        if (settings%nprior <= 0) then
+            nprior = settings%nlive
+        else
+            nprior = settings%nprior
         end if
         ndiscarded=0
 
@@ -45,38 +59,30 @@ def _fixture_source() -> str:
 #ifdef MPI
         else
             if(is_root(mpi_information)) then
-                active_workers=mpi_information%nprocs-1
-                do worker_id=1,active_workers
-                    live_point(settings%h0:settings%h1) = random_reals(settings%nDims)
-                    live_point(settings%b0) = ngenerated
-                    ngenerated = ngenerated+1
-                    call request_live_point(live_point,mpi_information,worker_id)
-                end do
                 do while(active_workers>0)
                     worker_id = catch_point(live_point,mpi_information)
                     if(RTI%nlive(1)<nprior) then
-                        live_point(settings%h0:settings%h1) = random_reals(settings%nDims)
-                        live_point(settings%b0) = ngenerated
-                        ngenerated = ngenerated+1
-                        call request_live_point(live_point,mpi_information,worker_id)
+                        call request_point(mpi_information,worker_id)
                     else
                         call no_more_points(mpi_information,worker_id)
                         active_workers=active_workers-1
                     end if
                 end do
             else
-                do while(live_point_needed(live_point,mpi_information))
+                do while(.true.)
+                    live_point(settings%h0:settings%h1) = random_reals(settings%nDims)
                     call calculate_point(loglikelihood,prior,live_point,settings,nlike)
                     ndiscarded=ndiscarded+1
                     call throw_point(live_point,mpi_information)
+                    if(.not. more_points_needed(mpi_information)) exit
                 end do
             end if
 #endif
         end if
 #ifdef MPI
-        nlike = sum_integers(nlike,mpi_information)
-        ndiscarded = sum_integers(ndiscarded,mpi_information)
-        total_time = sum_doubles(total_time,mpi_information)
+        nlike = sum_integers(nlike,mpi_information) ! Gather the likelihood calls onto one node
+        ndiscarded = sum_integers(ndiscarded,mpi_information) ! Gather the likelihood calls onto one node
+        total_time = sum_doubles(total_time,mpi_information) ! Sum up the total time taken
 #endif
         if(is_root(mpi_information)) then
             call write_finished_generating(settings%feedback)
@@ -90,9 +96,12 @@ def test_patch_module_and_markers_exist():
     module = _load_module()
     patched = module.patch_generate_source(_fixture_source())
     assert "POLYCHORD_BOOTSTRAP_V1" in patched
+    assert "POLYCHORD_BOOTSTRAP_RNG_V1" in patched
     assert "POLYCHORD_BOOTSTRAP_SEGMENT_VALID" in patched
     assert "write_bootstrap_checkpoint" in patched
     assert "read_bootstrap_checkpoint" in patched
+    assert "write_bootstrap_rng" in patched
+    assert "read_bootstrap_rng" in patched
     assert "bootstrap_target" in patched
     assert "stop 86" in patched
 
