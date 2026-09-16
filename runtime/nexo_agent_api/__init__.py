32e2f2ef665046b7d6d70357e61279c94d10cdbe
byDenoso <denosooo2@gmail.com>
2026-09-16 12:22:13 -0300

    feat: expose universal test registry

--- runtime/nexo_agent_api/__init__.py
+++ runtime/nexo_agent_api/__init__.py
@@ -3,10 +3,11 @@
 from .service import AgentService, TowerAgentIssue
 from .handoff import install_handoff_protocol
 from .ingress import install_request_ingress_protocol
+from .test_registry import register_test
 
 install_handoff_protocol(AgentService)
 install_request_ingress_protocol(AgentService)
 
 from .views import materialize_role_views
 
-__all__ = ["AgentService", "TowerAgentIssue", "materialize_role_views"]
+__all__ = ["AgentService", "TowerAgentIssue", "materialize_role_views", "register_test"]
